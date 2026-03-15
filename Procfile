b77e10c68b966e69839cf5f33338145421c6979d
chaitu2303 <chaitanyakumarsahu00@gmail.com>
2026-03-15 13:56:27 +0530

    fix: render port configuration

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -w 4 -k uvicorn.workers.UvicornWorker server:app
+web: gunicorn server:app -k uvicorn.workers.UvicornWorker --bind 0.0.0.0:$PORT
--- server.py
+++ server.py
@@ -487,4 +487,5 @@ app.add_middleware(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("server:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
